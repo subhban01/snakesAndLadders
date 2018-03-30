a902cd05ee722c3c6de725c15170bede1c3874a7
Subhadeep Banerjee <subhadeep.banerjee1@gmail.com>
2018-03-30 10:00:10 +0530

    snake & ladder boxes added

--- script.js
+++ script.js
@@ -2,7 +2,8 @@ const max = 6;
 const min = 1;
 var currentPos = 0;
 var prev = 0;
-const snakeLadderMap = {
+const delay = 3000;
+const ladderMap = {
 	4: 14,
 	9: 31,
 	20: 38,
@@ -10,7 +11,9 @@ const snakeLadderMap = {
 	40: 59,
 	51: 67,
 	63: 81,
-	71: 91,
+	71: 91
+}
+const snakesMap = {
 	17: 7,
 	54: 34,
 	62: 19,
@@ -21,29 +24,67 @@ const snakeLadderMap = {
 	99: 78
 }
 
+//initializing snake & ladder boxes
+snakeBoxes = Object.keys(snakesMap);
+for(var i in snakeBoxes){
+	$('.box')[parseInt(snakeBoxes[i])-1].classList.add('danger');
+}
+ladderBoxes = Object.keys(ladderMap);
+for(var i in ladderBoxes){
+	$('.box')[parseInt(ladderBoxes[i])-1].classList.add('ladder');
+}
+
+
 
 function rollDice(){
 	return Math.ceil(Math.random()*(max-min+1));
 }
 
 function play(){
+	// $('.roll').hide();
+	$('.roll').attr('onclick','').unbind('click');
+	$('.roll').text('please wait...');
+	$('.roll').css('opacity', '0.5');
+
 	var currentRoll = rollDice();
+	$('.display').text('You rolled '+currentRoll);
+	$('.display').show();
+	setTimeout(function(){
+		$('.display').fadeOut(delay);
+	}, 1000);
+
 	if(currentPos+currentRoll <= 100)
 		currentPos+=currentRoll;
 	
 	if(currentPos == 100)
 		console.log('You Win!!');
 
-	if(snakeLadderMap[currentPos]){
-		currentPos = snakeLadderMap[currentPos];
+	if(ladderMap[currentPos]){
+		var diff = ladderMap[currentPos] - currentPos;
+
+		currentPos = ladderMap[currentPos];
 	}
 
-	element = document.getElementsByTagName('div')[currentPos];
-		console.log('xxxx', currentPos);
+	if(snakesMap[currentPos]){
+		currentPos = snakesMap[currentPos];
+	}
+
+	element = document.getElementsByClassName('box')[currentPos-1];
+		console.log('roll', currentRoll, 'currentPos', currentPos);
 		element.classList.add("selected");
 
-		prevE = document.getElementsByTagName('div')[prev];
+		prevE = document.getElementsByClassName('box')[prev];
 		prevE.classList.remove("selected");
-		prev = currentPos;
-	return currentPos;
+		prev = currentPos-1;
+
+	setTimeout(function(){
+		$('.roll').css('opacity', '1');
+		$('.roll').text('ROLL DICE');
+		$('.roll').on('click', play);
+	},delay-1500);
 }
+
+
+
+
+
